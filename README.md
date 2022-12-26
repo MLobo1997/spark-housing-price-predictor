1f466634df071ff6356b7ac486da5ba40b8c4d38
MLobo1997 <miguel.lobo@feedzai.com>
2022-12-26 18:56:47 +0000

    Cleaning up main notebook

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Housing price predictor
 
+
+## Production Model
+
+### Noisy/Missing data
+
+One of the criteria of the assignment is that the service should be able to handle noisy data and missing values.
+The server application turns any invalid feature (e.g., a float with "3.%0") into a null and then the ML Pipeline performs imputation of every null with the feature average.
+These averages were computed with the training set.
+
+### Testing
+
+If you wish to test the application, I have added the collection [house_price_predictor.postman_collection.json](house_price_predictor.postman_collection.json) which you can simply import in 
+[Postman](https://www.postman.com/) to test the API. You can try removing features or adding noise to them.
+
 ## Further steps
--- house_price_predictor/features/zipcode_ranker.py
+++ house_price_predictor/features/zipcode_ranker.py
@@ -17,7 +17,50 @@ from pyspark.sql.types import IntegerType
 PERSISTENCE_FILE = "price_ranks.json"
 
 
+class ZipcodeRanker(Estimator):
+    """Computes the ranking of zipcodes by average price."""
+
+    def _fit(self, dataset: DataFrame) -> Transformer:
+        zipcodes_price_df = (
+            dataset.groupby("zipcode")
+            .agg(avg("price").alias("price_avg"))
+            .sort("price_avg", ascending=False)
+            .withColumn("zipcode_price_rank", monotonically_increasing_id())
+            .drop("price_avg")
+        )
+        return ZipCodeRankerModel(
+            {
+                row["zipcode"]: row["zipcode_price_rank"]
+                for row in zipcodes_price_df.collect()
+            }
+        )
+
+
+class ZipCodeRankerModel(Transformer, MLWritable, MLReadable["ZipCodeRankerModel"]):
+    """Creates a new column with the zipcode position within the ranking."""
+    def __init__(self, price_rank_per_zipcode_dict: Dict[int, int]) -> None:
+        super().__init__()
+        self.price_rank_per_zipcode_dict = price_rank_per_zipcode_dict
+
+    def _transform(self, dataset: DataFrame) -> DataFrame:
+        zipcode_rank_udf = udf(
+            lambda x: self.price_rank_per_zipcode_dict.get(x), IntegerType()
+        )
+        return dataset.withColumn(
+            "zipcode_price_rank", zipcode_rank_udf(col("zipcode"))
+        )
+
+    def write(self) -> MLWriter:
+        return ZipCodeRankerModelWriter(self)
+
+    @classmethod
+    def read(cls) -> MLReader[RL]:
+        return ZipCodeRankerModelReader()
+
+
 class ZipCodeRankerModelWriter(MLWriter):
+    """Necessary to make ZipcodeRankerModel serializable."""
+
     def __init__(self, instance: "ZipCodeRankerModel"):
         super().__init__()
         self.instance = instance
@@ -32,47 +75,11 @@ class ZipCodeRankerModelWriter(MLWriter):
 
 
 class ZipCodeRankerModelReader(MLReader):
+    """Necessary to make ZipcodeRankerModel serializable."""
+
     def load(self, path: str) -> RL:
         with open(f"{path}/{PERSISTENCE_FILE}", "r") as file:
             price_rank_per_zipcode_dict = json.load(file)
         return ZipCodeRankerModel(
             {int(k): int(v) for k, v in price_rank_per_zipcode_dict.items()}
-        )
-
-
-class ZipCodeRankerModel(Transformer, MLWritable, MLReadable["ZipCodeRankerModel"]):
-    def write(self) -> MLWriter:
-        return ZipCodeRankerModelWriter(self)
-
-    @classmethod
-    def read(cls) -> MLReader[RL]:
-        return ZipCodeRankerModelReader()
-
-    def __init__(self, price_rank_per_zipcode_dict: Dict[int, int]) -> None:
-        super().__init__()
-        self.price_rank_per_zipcode_dict = price_rank_per_zipcode_dict
-
-    def _transform(self, dataset: DataFrame) -> DataFrame:
-        zipcode_rank_udf = udf(
-            lambda x: self.price_rank_per_zipcode_dict.get(x), IntegerType()
-        )
-        return dataset.withColumn(
-            "zipcode_price_rank", zipcode_rank_udf(col("zipcode"))
-        )
-
-
-class ZipcodeRanker(Estimator):
-    def _fit(self, dataset: DataFrame) -> Transformer:
-        zipcodes_price_df = (
-            dataset.groupby("zipcode")
-            .agg(avg("price").alias("price_avg"))
-            .sort("price_avg", ascending=False)
-            .drop("price_avg")
-            .withColumn("zipcode_price_rank", monotonically_increasing_id())
-        )
-        return ZipCodeRankerModel(
-            {
-                row["zipcode"]: row["zipcode_price_rank"]
-                for row in zipcodes_price_df.collect()
-            }
         )
